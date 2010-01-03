d325f434d1dc222d41c6266b58cc91c775fe5290
Tobias Cohen <me@tobiascohen.com>
2010-01-03 22:20:53 +1100

    added twitter hashtag generation for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   has_and_belongs_to_many :interested_users, :class_name => 'User'
+  
+  def hash_tag
+    words = self.summary.downcase.gsub(/[^\s\w]+/, '').split(/\W+/)
+    while words.join('-').length > 20
+      words.pop
+    end
+    words.join '-'
+  end
 end
 
